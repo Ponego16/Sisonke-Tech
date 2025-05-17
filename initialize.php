657079b7124c0c370add95bcac07a69c0705cdad
Teka Mohlapa Ponego <mohlapateka@gmail.com>
2025-05-17 10:53:15 +0200

    Original files

---
+++ initialize.php
@@ -0,0 +1,10 @@
+<?php
+// $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://localhost/php-sms/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+// if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"sql.freedb.tech");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"freedb_sisonke tech");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"FUc76s2Y&AJ6$7u");
+if(!defined('DB_NAME')) define('DB_NAME',"freedb_SMS_DB");
+?>
