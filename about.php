2dc7a8ba4b1b884fa7d20b4324ad8f61499dfed0
SivuyileMakhubu <115796938+SivuyileMakhubu@users.noreply.github.com>
2025-05-20 22:31:16 +0200

    Update about.php
    I have changed language

--- about.php
+++ about.php
@@ -2,8 +2,29 @@
     <div class="container">
         <div class="card rounded-0">
             <div class="card-body">
-                <?= htmlspecialchars_decode(file_get_contents('./about.html')) ?>
-            </div>
-        </div>
+            <section class="py-5">
+    <div class="container">
+        <div class="card rounded-0">
+            <div class="card-body">
+                <h1 style="margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding: 0px; font-family: DauphinPlain; font-size: 70px; line-height: 90px; color: rgb(0, 0, 0); text-align: center;">About Us</h1><h4 style="margin: 10px 10px 5px; padding: 0px; font-size: 14px; line-height: 18px; text-align: center; font-style: italic; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif;">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h4><h5 style="margin: 5px 10px 20px; padding: 0px; font-size: 12px; line-height: 14px; text-align: center; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif;">"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h5><hr style="margin: 0px; padding: 0px; clear: both; border-top: 0px; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0)); color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; text-align: center;"><div id="Content" style="margin: 0px; padding: 0px; position: relative; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; text-align: center;"><div id="bannerL" style="margin: 0px 0px 0px -160px; padding: 0px; position: sticky; top: 20px; width: 160px; height: 10px; float: left; text-align: right;"></div><div id="bannerR" style="margin: 0px -160px 0px 0px; padding: 0px; position: sticky; top: 20px; width: 160px; height: 10px; float: right; text-align: left;"></div> <div class="boxed" style="margin: 10px 28.7969px; padding: 0px; clear: both;">
+  <div id="about-text" style="margin: 0px; padding: 0px; text-align: justify;">
+    <p style="margin: 0 0 15px 0; padding: 0;">
+      Welcome to our company! We are dedicated to providing exceptional service and innovative solutions that meet the needs of our clients. With a strong focus on quality and customer satisfaction, we strive to exceed expectations in everything we do.
+    </p>
+    <p style="margin: 0 0 15px 0; padding: 0;">
+      Our team consists of experienced professionals who are passionate about their work. We believe in continuous improvement, staying updated with the latest industry trends, and applying best practices to deliver outstanding results.
+    </p>
+    <p style="margin: 0 0 15px 0; padding: 0;">
+      Whether you are a new visitor or a long-time client, we value your interest and trust. We are committed to building strong, long-lasting relationships based on transparency, integrity, and mutual respect.
+    </p>
+    <p style="margin: 0 0 15px 0; padding: 0;">
+      Thank you for choosing us. We look forward to working with you and helping you achieve your goals through our services and expertise.
+    </p>
+  </div>
+</div>
+::after
+</div>           
+ </div>
+ </div>
     </div>
 </section>
