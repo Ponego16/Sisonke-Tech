346da496620daf64bd3cc0a2ccea44afc7546fd0
SivuyileMakhubu <115796938+SivuyileMakhubu@users.noreply.github.com>
2025-05-21 08:57:13 +0200

    Update about.php

--- about.php
+++ about.php
@@ -9,7 +9,7 @@
             <div id="bannerL" style="margin: 0px 0px 0px -160px; padding: 0px; position: sticky; top: 20px; width: 160px; height: 10px; float: left; text-align: right;"></div>
             <div id="bannerL" style="margin: 0px 0px 0px -160px; padding: 0px; position: sticky; top: 20px; width: 160px; height: 10px; float: left; text-align: right;"></div>
             <div id="bannerR" style="margin: 0px -160px 0px 0px; padding: 0px; position: sticky; top: 20px; width: 160px; height: 10px; float: right; text-align: left;"></div>
-            <div class="boxed" style="margin: 10px 28.7969px; padding: 0px; clear: both;">
+            < class="boxed" style="margin: 10px 28.7969px; padding: 0px; clear: both;">
   <div id="about-text" style="margin: 0px; padding: 0px; text-align: justify;">
     <p style="margin: 0 0 15px 0; padding: 0;">
       Welcome to our company! We are dedicated to providing exceptional service and innovative solutions that meet the needs of our clients. With a strong focus on quality and customer satisfaction, we strive to exceed expectations in everything we do.
@@ -24,21 +24,6 @@
       Thank you for choosing us. We look forward to working with you and helping you achieve your goals through our services and expertise.
     </p>
   </div>
-</div>
-<div class="boxed" style="margin: 10px 28.7969px; padding: 0px; clear: both;">
-  <div id="about-text" style="margin: 0px; padding: 0px; text-align: justify;">
-    <p style="margin: 0 0 15px 0; padding: 0;">
-      Welcome to our company! We are dedicated to providing exceptional service and innovative solutions that meet the needs of our clients. With a strong focus on quality and customer satisfaction, we strive to exceed expectations in everything we do.
-    </p>
-    <p style="margin: 0 0 15px 0; padding: 0;">
-      Our team consists of experienced professionals who are passionate about their work. We believe in continuous improvement, staying updated with the latest industry trends, and applying best practices to deliver outstanding results.
-    </p>
-    <p style="margin: 0 0 15px 0; padding: 0;">
-      Whether you are a new visitor or a long-time client, we value your interest and trust. We are committed to building strong, long-lasting relationships based on transparency, integrity, and mutual respect.
-    </p>
-    <p style="margin: 0 0 15px 0; padding: 0;">
-      Thank you for choosing us. We look forward to working with you and helping you achieve your goals through our services and expertise.
-    </p>
   </div>
 </div>
 ::after
